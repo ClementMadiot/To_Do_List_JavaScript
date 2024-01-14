eac92a315591390e7b3fe394e1531aae4704e2a5
ClementMadiot <clementmadiot09@gmail.com>
2024-01-14 10:57:26 +0100

    To Do List functional

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ var addToDoButton = document.getElementById('addToDo');
 var toDoContainer = document.getElementById('toDoContainer');
 var inputField = document.getElementById('inputField');
 
-// Evenement au click
+//* Évenement au click
 
 addToDoButton.onclick = function(){
-  // si l'input n'est pas vide, créer un paragraphe
-  if(inputField.value !=''){
-
+  /// si l'input n'est pas vide, créer un paragraphe
+  if(inputField.value != ""){
+    var paragraph = document.createElement('p')
   }
+  /// Valorisé la variable avec le contenu de l'utilisateur
+  paragraph.innerText = inputField.value;
+
+  //* Stylisé paragraph
+  paragraph.classList.add('paragraph_style')
+  toDoContainer.appendChild(paragraph);
+
+  /// Vider l'input quand le paragraph est ajouté
+  inputField.value = "";
+
+  //* Barré le paragraph au click
+  paragraph.addEventListener('click',function(){
+    paragraph.classList.add('paragraph_click')
+  })
+
+  //* Supprimer tâche au double click
+
+  paragraph.addEventListener('dblclick',function(){
+    toDoContainer.removeChild(paragraph);
+  })
+
 }
