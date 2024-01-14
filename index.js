d8a6b16d7e5098f2fcacd56f152601feffabf739
ClementMadiot <clementmadiot09@gmail.com>
2024-01-14 10:24:32 +0100

    html and style CSS done

---
+++ index.js
@@ -0,0 +1,12 @@
+var addToDoButton = document.getElementById('addToDo');
+var toDoContainer = document.getElementById('toDoContainer');
+var inputField = document.getElementById('inputField');
+
+// Evenement au click
+
+addToDoButton.onclick = function(){
+  // si l'input n'est pas vide, créer un paragraphe
+  if(inputField.value !=''){
+
+  }
+}
